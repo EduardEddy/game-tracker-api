71d54ce0e3792b7ccec5011cb08a64a720dcc7f4
eduard <Eddy.acevedo@crediviva.com.pa>
2023-10-10 14:42:27 -0500

    remove duplicate route name

--- routes/attractions/index.php
+++ routes/attractions/index.php
@@ -4,8 +4,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Attractions\AttractionController;
 
 Route::controller(AttractionController::class)->group(function() {
-    Route::get('/attractions', 'index')->name('attractions');
-    Route::post('/attractions','store')->name('attractions');
+    Route::get('/attractions', 'index');
+    Route::post('/attractions','store');
     Route::put('/attractions/{attraction}', 'update');
     Route::get('/attractions/{attraction}', 'show');
     Route::delete('/attractions/{attraction}', 'destroy')->name('attractions.destroy');
