57406de7c420f9257d470dd82db52be88867235f
eduard <Eddy.acevedo@crediviva.com.pa>
2023-10-14 22:39:33 -0500

    add attraction name to route

--- routes/attractions/index.php
+++ routes/attractions/index.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Attractions\AttractionController;
 
 Route::controller(AttractionController::class)->group(function() {
-    Route::get('/attractions', 'index');
+    Route::get('/attractions', 'index')->name('attractions');
     Route::post('/attractions','store');
     Route::put('/attractions/{attraction}', 'update');
     Route::get('/attractions/{attraction}', 'show');
